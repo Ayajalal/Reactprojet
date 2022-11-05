1393885398838f41e088aeb10f25872b67bc0f53
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2022-11-05 14:58:39 +0200

    Carousel and navbar and register

--- recipe-project/src/App.js
+++ recipe-project/src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchRecipe from "./Componant/search-recipe";
+import Navbar from "./Componant/Navbar";
+import Header from "./Componant/Header";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import Register from "./Componant/Register";
+import Login from "./Componant/Login";
 
 function App() {
-  return <SearchRecipe />;
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  useEffect(() => {}, []);
+
+  return (
+    <React.Fragment>
+      <Navbar />
+      {loggedUser ? (
+        <>
+          <Carousel infiniteLoop showStatus={false} autoPlay interval={5000}>
+            <Header imageUrl="/assets/images/recipes-header-bg.jpg" />
+            <Header imageUrl="/assets/images/recipes-header-bg1.jpg" />
+            <Header imageUrl="/assets/images/recipes-header-bg2.jpg" />
+          </Carousel>
+
+          <SearchRecipe />
+        </>
+      ) : (
+        <>
+          <Register />
+          <Login />
+        </>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default App;
